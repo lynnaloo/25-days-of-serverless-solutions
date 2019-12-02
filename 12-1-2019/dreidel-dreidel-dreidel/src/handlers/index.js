4f1bfa74f8dfc699056a72b2f381222a2aa143cb
Linda Nichols <lynnaloo@gmail.com>
2019-12-02 16:05:28 -0500

    [initial] First commit

---
+++ 12-1-2019/dreidel-dreidel-dreidel/src/handlers/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports.spin = async function(context, req) {
+  context.log('The dreidel is spinning!');
+
+  const randomValue = Math.floor(Math.random() * 3);
+  const driedel = [' נ (Nun)',' ג (Gimmel)',' ה (Hay)',' ש (Shin)']
+  context.res = {
+      body: values[randomValue]
+  };
+};
