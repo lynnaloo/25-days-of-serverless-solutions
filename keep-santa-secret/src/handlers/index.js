feb13f5f6443a587a0c66cb8673383d46c7bfb58
Linda Nichols <lynnaloo@gmail.com>
2019-12-03 01:25:04 -0500

    [update] Add scaffolding for exercise 3

---
+++ keep-santa-secret/src/handlers/index.js
@@ -0,0 +1,29 @@
+
+module.exports.saveGift = async function(context, req) {
+  context.log('Saving URL to database');
+
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  if (req.body) {
+    const input = req.body;
+
+    const timestamp = Date.now();
+
+    const output = JSON.stringify({
+      url: input.url,
+      timestamp
+    });
+    // context.bindings.record = output;
+    // context.log('Finish writing to CosmosDB');
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: output
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Please pass a name on the query string or in the request body'
+    };
+  }
+};
